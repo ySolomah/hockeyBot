fa02e225206e4b12db6887bd707b9b71b91a485d
ySolomah <yassir.solomah@gmail.com>
2017-05-16 00:18:33 -0400

    Add files via upload
    config files from original

---
+++ config.py
@@ -0,0 +1,14 @@
+reddit = {
+	"username": "USERNAME_HERE",
+	"password": "PASSWORD_HERE",
+	"client_id": "ID_HERE",
+	"client_secret": "SECRET_HERE"
+}
+
+firebase = {
+	"apiKey": "API_KEY_HERE",
+	"authDomain": "DOMAIN_HERE",
+	"databaseURL": "FIREBASE_URL_HERE",
+	"storageBucket": "STORAGE_BUCKET_HERE",
+	"serviceAccount": "SERVICE_ACCOUNT_JSON_HERE"
+}
